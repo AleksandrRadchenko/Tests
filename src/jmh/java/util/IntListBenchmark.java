21a758a3d0e530b8786547aace4f073a3c2d51b3
Aleksandr Radchenko <partner.ar@gmail.com>
2020-02-25 16:30:09 +0300

    RadixSort - fix OOME in benchmark, add FastUtils

--- src/jmh/java/util/IntListBenchmark.java
+++ src/jmh/java/util/IntListBenchmark.java
@@ -1,5 +1,6 @@
 package util;
 
+import it.unimi.dsi.fastutil.ints.IntArrayList;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
@@ -7,29 +8,24 @@ import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.profile.StackProfiler;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
-import static util.IntListProfiling.SIZE;
-import static util.IntListProfiling.SOURCE_ARRAY;
-import static util.IntListProfiling.SOURCE_LIST;
-
 /**
- * Benchmark                                  Mode  Cnt     Score     Error  Units
- * IntListBenchmark.arrayListAddAllBench      avgt    5  2229.842 ± 915.490  ms/op
- * IntListBenchmark.arrayListAddBench         avgt    5     0.058 ±   0.015  ms/op
- * IntListBenchmark.arrayListAddUnboxedBench  avgt    5     0.060 ±   0.010  ms/op
- * IntListBenchmark.intListAddAllBench        avgt    5     0.004 ±   0.001  ms/op
- * IntListBenchmark.intListAddBench           avgt    5     0.045 ±   0.023  ms/op
+ * Benchmark                      Mode  Cnt    Score    Error  Units
+ * IntListBenchmark.arrayListAdd  avgt   15  211.780 �  4.041  ns/op
+ * IntListBenchmark.intListAdd    avgt   15   94.285 � 13.331  ns/op
  */
 @SuppressWarnings({"unused"})
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
@@ -37,57 +33,75 @@ import static util.IntListProfiling.SOURCE_LIST;
 @Fork(1)
 @Measurement(iterations = 5, time = 1)
 @Warmup(iterations = 2, time = 1)
-@State(Scope.Benchmark)
 public class IntListBenchmark {
+    private static final int SIZE = 1 << 19; //1 << 29 = 536,870,912
+    private static final int UNIT = 1 << 20; //1,048,576
     private static final Random RND = new Random();
+    private static final int[] SOURCE_ARRAY = RND.ints(SIZE).toArray();
+
+    @State(Scope.Benchmark)
+    public static class IntListState {
+        final IntList DESTINATION = new IntList(SIZE);
+    }
+
+    @State(Scope.Benchmark)
+    public static class ArrayListState {
+        final ArrayList<Integer> SOURCE_LIST = new ArrayList<>();
+        final Integer[] SOURCE_ARRAY_BOXED = new Integer[SIZE];
+        @Setup
+        public void setup() {
+            for (int i = 0; i < SOURCE_ARRAY.length; i++) {
+                SOURCE_LIST.add(SOURCE_ARRAY[i]);
+                //noinspection UnnecessaryBoxing
+                SOURCE_ARRAY_BOXED[i] = Integer.valueOf(SOURCE_ARRAY[i]);
+            }
+        }
+        final ArrayList<Integer> DESTINATION = new ArrayList<>(SIZE);
+    }
+
+    @State(Scope.Benchmark)
+    public static class IntArrayListState {
+        final IntArrayList DESTINATION = new IntArrayList(SIZE);
+    }
 
     public static void main(String[] args) throws RunnerException {
         Options opt = new OptionsBuilder()
                 .include(IntListBenchmark.class.getSimpleName())
+                .addProfiler(StackProfiler.class, "lines=1;period=1")
+                .jvmArgs("-XX:+HeapDumpOnOutOfMemoryError")
                 .build();
         new Runner(opt).run();
     }
 
     @Benchmark
-    public static IntList intListAddBench() {
-        IntList intList = new IntList();
+    public void intListAdd(IntListState state, Blackhole bh) {
         for (int i = 0; i < SIZE; i++) {
-            intList.add(SOURCE_ARRAY[i]);
+            bh.consume(state.DESTINATION.add(SOURCE_ARRAY[i]));
         }
-        return intList;
+        state.DESTINATION.clear(); // takes about 2% CPU time
     }
 
     @Benchmark
-    public static IntList intListAddAllBench() {
-        IntList intList = new IntList();
-        intList.addAll(SOURCE_ARRAY);
-        return intList;
+    public void intArrayListAdd(IntArrayListState state, Blackhole bh) {
+        for (int i = 0; i < SIZE; i++) {
+            bh.consume(state.DESTINATION.add(SOURCE_ARRAY[i]));
+        }
+        state.DESTINATION.clear();
     }
 
     @Benchmark
-    public static List<Integer> arrayListAddBench() {
-        ArrayList<Integer> integers = new ArrayList<>();
+    public void arrayListAddPrimitive(ArrayListState state, Blackhole bh) {
         for (int i = 0; i < SIZE; i++) {
-            integers.add(SOURCE_LIST.get(i));
+            bh.consume(state.DESTINATION.add(SOURCE_ARRAY[i]));
         }
-        return integers;
+        state.DESTINATION.clear();
     }
 
     @Benchmark
-    public static List<Integer> arrayListAddUnboxedBench() {
-        ArrayList<Integer> integers = new ArrayList<>();
+    public void arrayListAddBoxed(ArrayListState state, Blackhole bh) {
         for (int i = 0; i < SIZE; i++) {
-            integers.add(SOURCE_ARRAY[i]);
+            bh.consume(state.DESTINATION.add(state.SOURCE_ARRAY_BOXED[i]));
         }
-        return integers;
-    }
-
-    @Benchmark
-    public static List<Integer> arrayListAddAllBench() {
-        ArrayList<Integer> integers = new ArrayList<>();
-        for (int i = 0; i < SIZE; i++) {
-            integers.addAll(SOURCE_LIST);
-        }
-        return integers;
+        state.DESTINATION.clear();
     }
 }
--- src/jmh/java/util/IntListProfiling.java
+++ src/jmh/java/util/IntListProfiling.java
@@ -1,49 +1,28 @@
 package util;
 
-import org.openjdk.jol.info.ClassLayout;
-import org.openjdk.jol.info.GraphLayout;
-import org.openjdk.jol.layouters.CurrentLayouter;
-
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
 
 public class IntListProfiling {
-    public static final Random RND = new Random();
-    public static final int SIZE = 10000;
-    public static final int[] SOURCE_ARRAY = RND.ints(SIZE).toArray();
-    public static final ArrayList<Integer> SOURCE_LIST = new ArrayList<>();
-    static {
-        for (int i : SOURCE_ARRAY) {
-            SOURCE_LIST.add(i);
-        }
-    }
+    private static final int SIZE = 1 << 29; //1 << 29 = 536,870,912
+    private static final int UNIT = 1 << 20; //1,048,576
+    private static final IntList INT_LIST = new IntList(SIZE);
+    private static final ArrayList<Integer> ARRAY_LIST = new ArrayList<>(SIZE);
 
     public static void main(String[] args) {
-        System.out.println(ClassLayout.parseClass(int[].class, new CurrentLayouter()).toPrintable(intListAddBench().getElementData()));
-        System.out.println(ClassLayout.parseClass(ArrayList.class, new CurrentLayouter()).toPrintable(arrayListAddBench()));
-        System.out.println(GraphLayout.parseInstance(intListAddBench()).toFootprint());
-        System.out.println(GraphLayout.parseInstance(arrayListAddBench()).toFootprint());
-        //noinspection InfiniteLoopStatement
-        while (true) {
-            IntListBenchmark.arrayListAddBench();
+        System.out.println("Max heap: " + Runtime.getRuntime().maxMemory()/(UNIT) + "Mb");
+        for (int i = 0; i < SIZE; i++) {
+            intListAddBench();
+//            arrayListAddBench(); // OOME for 1<<29 size
         }
+        System.out.println("Remaining heap: " + Runtime.getRuntime().freeMemory()/(UNIT) + "Mb"  );
     }
 
-    public static IntList intListAddBench() {
-        IntList intList = new IntList();
-        for (int i : SOURCE_ARRAY) {
-            intList.add(i);
-        }
-        return intList;
+    public static boolean intListAddBench() {
+        return INT_LIST.add(42);
     }
 
-    public static List<Integer> arrayListAddBench() {
-        ArrayList<Integer> integers = new ArrayList<>();
-        //noinspection CollectionAddAllCanBeReplacedWithConstructor
-        integers.addAll(SOURCE_LIST);
-        return integers;
+    public static boolean arrayListAddBench() {
+        return ARRAY_LIST.add(42);
     }
 
 }
